0cb22a3203f107b95d11457ca0924cec85441651
rwishd17 <rwishd17@gmail.com>
2023-02-01 15:03:50 +0300

    ChakraUI-Lab

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Footer from './Components/Footer'
+import Nav from './Components/Nav'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Nav/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
